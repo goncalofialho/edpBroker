a871432536db6dfeabc8c7bfc09546c0e21b6cb4
unknown <ist179112@lab3p5.ad.rnl.tecnico.ulisboa.pt>
2017-11-21 15:24:51 +0000

    buy menu almost done

--- js/intro.js
+++ js/intro.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){
 	$('.info .verify-energy').click(toggleView);
 	$('#intro .car-icon').click(toggleView);
+	$('.header .home').click(toggleView);
+	$('.header .back').click(toggleView);
 
+	/* BUY ENERGY */ 
+	$('#buy-energy #main-buy-energy .packages-list .packs .pack').click(toggleBuyPackets)
 })
 
 function toggleView(){
-	console.log("123123")
   viewId = $(this).attr('target')
   /* ESCONDER TODOS AS VIEWS */
   $('.menu').addClass('hidden-class')
   $('.content').addClass('hidden-class')
   $('.content#'+viewId).removeClass('hidden-class')
   
-  /* ESCONDER TODOS OS HEADERS 
+  /* ESCONDER TODOS OS HEADERS  */
+  console.log(viewId)
   $('.header').addClass('hidden-class')
-  $('.header[target="'+viewId+'"]').removeClass('hidden-class')*/
+  $('.header[target="'+viewId+'"]').removeClass('hidden-class')
+  if(viewId == "intro")
+	  $('.menu').removeClass('hidden-class')
 }
 
 
+function toggleBuyPackets(){
+	viewId = $(this).attr('target')
+	console.log($(this))
+	console.log(viewId)
+	$('#buy-energy > div').addClass('hidden-class');
+	$('#buy-energy #'+viewId).removeClass('hidden-class');
+}
