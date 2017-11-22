ccbc3ed9270fc2bb7538f1b2c7d7256f8bbfe774
Gonçalo Fialho <goncalo.f.pires@hotmail.com>
2017-11-22 10:58:20 +0000

    buy energy done

--- js/intro.js
+++ js/intro.js
@@ -2,32 +2,52 @@ $(document).ready(function(){
 	$('.info .verify-energy').click(toggleView);
 	$('#intro .car-icon').click(toggleView);
 	$('.header .home').click(toggleView);
-	$('.header .back').click(toggleView);
+	$('.header .back').click(toggleBackView);
 
-	/* BUY ENERGY */ 
+	/* BUY ENERGY */
 	$('#buy-energy #main-buy-energy .packages-list .packs .pack').click(toggleBuyPackets)
 })
 
 function toggleView(){
   viewId = $(this).attr('target')
+	console.log("1" , $(this))
   /* ESCONDER TODOS AS VIEWS */
   $('.menu').addClass('hidden-class')
   $('.content').addClass('hidden-class')
   $('.content#'+viewId).removeClass('hidden-class')
-  
+
   /* ESCONDER TODOS OS HEADERS  */
-  console.log(viewId)
   $('.header').addClass('hidden-class')
   $('.header[target="'+viewId+'"]').removeClass('hidden-class')
   if(viewId == "intro")
 	  $('.menu').removeClass('hidden-class')
 }
 
+/* VERIFICA SE VOLTAR PARA TRAS É SUBVIEW OU VIEW PRINCIPAL */
+function toggleBackView(){
+	if(! $('.content:not(.hidden-class) > *').hasClass('hidden-class')){
+		goToIntro()
+	}else{
+		el = $('.content:not(.hidden-class)')
+		index = $('#buy-energy > div').index($('#buy-energy > div:not(.hidden-class)'))
+		if(index-1 == -1){
+			goToIntro()
+		}else{
+			$('.content:not(.hidden-class) > div').addClass('hidden-class')
+			$(el.children().get(index-1)).removeClass('hidden-class')
+		}
+	}
+}
+function goToIntro(){
+	$('.menu').removeClass('hidden-class')
+	$('.content').addClass('hidden-class')
+	$('.content#intro').removeClass('hidden-class')
+	$('.header').addClass('hidden-class')
+	$('.header[target="intro"]').removeClass('hidden-class')
+}
 
 function toggleBuyPackets(){
 	viewId = $(this).attr('target')
-	console.log($(this))
-	console.log(viewId)
 	$('#buy-energy > div').addClass('hidden-class');
 	$('#buy-energy #'+viewId).removeClass('hidden-class');
 }
