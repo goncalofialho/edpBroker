c6a53557b80e28c3f21997ea71e9ff0bbd17de33
Gonçalo Fialho <goncalo.f.pires@hotmail.com>
2017-11-29 18:43:30 +0000

    js error

--- js/login.js
+++ js/login.js
@@ -4,10 +4,35 @@ $(document).ready(function(){
     $(this).find('span.checkBox-icon').toggleClass('checked')
   })
 
-  $.getJSON( 'http://85.247.219.175:4567/api/customers/login?email='+mail+'&password='+psswd+'', function( data ) {
-    console.log(data.length)
 
-  });
+  $('.btn.btn-primary').click(trylogin)
+
+
+
+
+
+
 })
+
+var x;
+var json;
+function trylogin(){
+
+  mail = $('.login-input-box.user .login-input').val()
+  psswd = $('.login-input-box.password .login-input').val()
+  console.log('http://85.247.219.175:4567/api/customers/login?email='+mail+'&password='+psswd+'')
+
+  $.getJSON( 'http://85.247.219.175:4567/api/customers/login?email='+mail+'&password='+psswd+'').done(function(data){
+    x = data
+    json = x["responseJSON"]
+    console.log(x)
+    if(x == null){
+      $('.formValidatorsBox.hidden').removeClass('hidden')
+      $('.formValidatorsBox .erro .msgTxt').text("Dados inválidos")
+    }else{
+      console.log(x["customer_id"][0])
+    }
+  })
+}
 
 // http://85.247.219.175:4567/api/customers?filter={"where":{"customer_id":10}}
